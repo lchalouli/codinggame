0c26294df943447ee53284750fae639caff8c5ad
Chalouli <Lela>
2016-09-02 17:17:12 +0200

    DIRECT_S : bon generator

--- src/main/java/com/leila/codinggame/GreatestNumberTest.java
+++ src/main/java/com/leila/codinggame/GreatestNumberTest.java
@@ -1,5 +1,8 @@
 package com.leila.codinggame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,19 +13,25 @@ public class GreatestNumberTest {
 
     @Test
     public void test() {
+        String[] tab = new String[]{".","-","1","2","3","4","5","6","7","8","9"};
         final Stream<String> stringStream = new Random(2).ints().limit(100)
                 .map(operand -> Math.abs(operand))
                 .mapToObj(value -> String.valueOf(value));
 
-        stringStream.map(s -> s.chars().mapToObj(operand -> getRandomChar(operand)).collect(Collectors.joining())).forEach(s1 -> System.out.println(s1));
+        stringStream.map(s -> {
+            final ArrayList<String> tabCopy = new ArrayList<>(Arrays.asList(tab));
+            return s.chars().mapToObj(operand -> getRandomChar(operand, tabCopy)).collect(Collectors.joining());
+        }).forEach(s1 -> System.out.println(s1));
     }
 
-    private String getRandomChar(int random) {
-        String[] tab = new String[]{".","-","1","2","3","4","5","6","7","8","9"};
-        final double index = random % tab.length;
+    private String getRandomChar(int random, List<String> tab) {
+        final double index = random % tab.size();
         final int rounded = Long.valueOf(Math.round(index)).intValue();
-        System.out.println(index);
-        return tab[rounded];
+        final String character = tab.get(rounded);
+        if (character.equals(".") || character.equals("-")) {
+            tab.remove(character);
+        }
+        return character;
     }
 
 }
